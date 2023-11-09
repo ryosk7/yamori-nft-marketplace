1d2df10c809013287b1b7a14b8729ad381852897
ryosk7 <uchiryo7@gmail.com>
2023-11-09 10:23:59 +0900

    feat: chapter-6, Created NFT collection show page.

--- app/models/nft_collection.rb
+++ app/models/nft_collection.rb
@@ -1,2 +1,6 @@
 class NftCollection < ApplicationRecord
+  def self.replaced_ipfs_image_url(ipfs_image_link)
+    return ipfs_image_link if ipfs_image_link.exclude?('ipfs://')
+    "http://ipfs.io/ipfs/#{ipfs_image_link.sub('ipfs://', '')}"
+  end
 end
